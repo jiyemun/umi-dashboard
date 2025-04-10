919636320dbb5fbd4616bd57fc55631f6dbb8f38
jiyemun <jym6853@gmail.com>
2025-04-10 19:45:12 +0900

    Style: favicon add

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 // 运行时配置
-
+import favicon from '../public/favicon.ico';
 // 全局初始化数据配置，用于 Layout 用户信息和权限初始化
 // 更多信息见文档：https://umijs.org/docs/api/runtime-config#getinitialstate
 export async function getInitialState(): Promise<{ name: string }> {
@@ -8,7 +8,7 @@ export async function getInitialState(): Promise<{ name: string }> {
 
 export const layout = () => {
   return {
-    logo: 'https://img.alicdn.com/tfs/TB1YHEpwUT1gK0jSZFhXXaAtVXa-28-27.svg',
+    logo: favicon,
     menu: {
       locale: false,
     },
