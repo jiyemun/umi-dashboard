f0b1e0d8f05859c20d793eb941379d84333dd9ba
jiyemun <jym6853@gmail.com>
2025-04-12 01:40:56 +0900

    Updates

---
+++ .umirc.ts
@@ -0,0 +1,51 @@
+import { defineConfig } from '@umijs/max';
+
+export default defineConfig({
+  antd: {},
+  access: {},
+  model: {},
+  initialState: {},
+  request: {},
+  layout: {
+    title: 'TACHYON',
+  },
+  locale: {
+    antd: true,
+    baseNavigator: false,
+    baseSeparator: '-',
+    default: 'en-US',
+    title: false,
+    useLocalStorage: false,
+  },
+  favicons: ['/favicon.ico'],
+  routes: [
+    {
+      path: '/',
+      redirect: '/home',
+    },
+    {
+      name: 'Home',
+      path: '/home',
+      component: './Home',
+    },
+    {
+      name: 'Projects',
+      path: '/projects',
+      component: './Projects',
+    },
+    {
+      name: 'Profile',
+      path: '/profile',
+      component: './Profile',
+    },
+  ],
+
+  npmClient: 'npm',
+  alias: {
+    '@': require('path').resolve(__dirname, 'src'),
+  },
+  define: {
+    'process.env': process.env, // ✅ 환경 변수 사용 가능하게 설정
+  },
+  tailwindcss: {},
+});
